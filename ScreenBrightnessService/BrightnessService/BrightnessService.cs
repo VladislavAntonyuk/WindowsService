260bed8ebe9c2e2ceb2cf66e97108cc00567eaa8
Vladislav Antonyuk <vlad.antonyuk@outlook.com>
2021-01-10 17:46:30 +0200

    Check brightness before set

--- ScreenBrightnessService/BrightnessService/BrightnessService.cs
+++ ScreenBrightnessService/BrightnessService/BrightnessService.cs
@@ -4,17 +4,40 @@ namespace ScreenBrightnessService.BrightnessService
 {
 	internal class BrightnessService
 	{
-		public static void SetBrightnessLaptop(int brightness)
+		public static byte GetBrightness()
 		{
-			using var managementClass = new ManagementClass("WmiMonitorBrightnessMethods")
+			var managementObjectSearcher = new ManagementObjectSearcher(new ManagementScope("root\\WMI"),
+				new SelectQuery("WmiMonitorBrightness"));
+			var objectCollection = managementObjectSearcher.Get();
+			byte num = 0;
+			using (var enumerator = objectCollection.GetEnumerator())
 			{
-				Scope = new ManagementScope(@"\\.\root\wmi")
-			};
-			using var instances = managementClass.GetInstances();
-			var args = new object[] {1, brightness};
-			foreach (var instance in instances)
-				if (instance is ManagementObject managementObject)
-					managementObject.InvokeMethod("WmiSetBrightness", args);
+				if (enumerator.MoveNext())
+					num = (byte) enumerator.Current.GetPropertyValue("CurrentBrightness");
+			}
+
+			objectCollection.Dispose();
+			managementObjectSearcher.Dispose();
+			return num;
+		}
+
+		public static void SetBrightness(byte targetBrightness)
+		{
+			var managementObjectSearcher = new ManagementObjectSearcher(new ManagementScope("root\\WMI"),
+				new SelectQuery("WmiMonitorBrightnessMethods"));
+			var objectCollection = managementObjectSearcher.Get();
+			using (var enumerator = objectCollection.GetEnumerator())
+			{
+				if (enumerator.MoveNext())
+					((ManagementObject) enumerator.Current).InvokeMethod("WmiSetBrightness", new object[]
+					{
+						uint.MaxValue,
+						targetBrightness
+					});
+			}
+
+			objectCollection.Dispose();
+			managementObjectSearcher.Dispose();
 		}
 	}
 }
--- ScreenBrightnessService/Worker.cs
+++ ScreenBrightnessService/Worker.cs
@@ -9,16 +9,24 @@ namespace ScreenBrightnessService
 	{
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
+			const byte minBrightness = 0;
+			const byte maxBrightness = 100;
 			while (!stoppingToken.IsCancellationRequested)
 			{
 				var powerState = PowerState.GetPowerState();
 				if (powerState.BatteryFlag == BatteryFlag.NoSystemBattery)
 					await StopAsync(stoppingToken);
 				else
-					BrightnessService.BrightnessService.SetBrightnessLaptop(
-						powerState.ACLineStatus == AcLineStatus.Offline
-							? 0
-							: 100);
+				{
+					var currentBrightness = BrightnessService.BrightnessService.GetBrightness();
+					var desiredBrightness = powerState.ACLineStatus == AcLineStatus.Offline
+						? minBrightness
+						: maxBrightness;
+					if (currentBrightness != desiredBrightness)
+					{
+						BrightnessService.BrightnessService.SetBrightness(desiredBrightness);
+					}
+				}
 
 				await Task.Delay(1000, stoppingToken);
 			}
